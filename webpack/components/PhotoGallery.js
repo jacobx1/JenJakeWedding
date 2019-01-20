87aaab3b89f186c81f1380c45713759ae9057e9b
Jacob Schwind <jacobx1@gmail.com>
2019-01-20 14:07:38 -0500

    Taiwan Reception (#13)
    * Taiwan Reception
    
    - Added kiss from 2018 reception
    - Added Howard hotel info
    
    * Add Taiwan translation
    
    * Update gallery
    
    * Security update

--- webpack/components/PhotoGallery.js
+++ webpack/components/PhotoGallery.js
@@ -16,23 +16,23 @@ export default class PhotoGallery extends React.Component {
   openLightbox(event, obj) {
     this.setState({
       currentImage: obj.index,
-      lightboxIsOpen: true
+      lightboxIsOpen: true,
     });
   }
   closeLightbox() {
     this.setState({
       currentImage: 0,
-      lightboxIsOpen: false
+      lightboxIsOpen: false,
     });
   }
   gotoPrevious() {
     this.setState({
-      currentImage: this.state.currentImage - 1
+      currentImage: this.state.currentImage - 1,
     });
   }
   gotoNext() {
     this.setState({
-      currentImage: this.state.currentImage + 1
+      currentImage: this.state.currentImage + 1,
     });
   }
   render() {
@@ -54,22 +54,17 @@ export default class PhotoGallery extends React.Component {
 }
 
 const PHOTO_SET = [
-  { src: 'img/j_stockton_20150729_8873.jpg' },
+  { src: 'img/IMG_0437-mini.jpg' },
   { src: 'img/j_stockton_20150729_8878.jpg', width: 2, height: 3 },
-  //{src: 'img/j_stockton_20150729_8883.jpg'},
-  { src: 'img/j_stockton_20150729_8887.jpg' },
+  { src: 'img/IMG_0483-mini.jpg' },
+
   { src: 'img/j_stockton_20150729_8897.jpg', width: 2, height: 3 },
-  //{src: 'img/j_stockton_20150729_8905.jpg', width: 2, height: 3},
-  { src: 'img/j_stockton_20150729_8918.jpg', width: 2, height: 3 },
+  { src: 'img/IMG_0476-mini.jpg', width: 2, height: 3 },
   { src: 'img/j_stockton_20150729_8920.jpg', width: 2, height: 3 },
-  //{src: 'img/j_stockton_20150729_8922.jpg'},
-  { src: 'img/j_stockton_20150729_8940.jpg', width: 2, height: 3 },
+
+  { src: 'img/j_stockton_20160510_1957-mini.jpg' },
   { src: 'img/j_stockton_20150729_8956.jpg', width: 2, height: 3 },
-  { src: 'img/j_stockton_20150729_8958.jpg' }
-  //{src: 'img/j_stockton_20150729_8974.jpg'},
-  //{src: 'img/j_stockton_20150729_8999.jpg'},
-  //{src: 'img/j_stockton_20150729_9048.jpg', width: 2, height: 3},
-  //{src: 'img/j_stockton_20150729_9062.jpg', width: 2, height: 3}
+  { src: 'img/j_stockton_20160510_2354-2-mini.jpg' },
 ];
 
 PHOTO_SET.map(item => {
