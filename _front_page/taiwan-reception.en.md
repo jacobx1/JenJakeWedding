fe9c304a0c16111dbeb38a71365a9cdc1cdc5035
Jacob Schwind <jacobx1@gmail.com>
2019-01-20 13:10:01 -0500

    Taiwan Reception
    - Added kiss from 2018 reception
    - Added Howard hotel info

---
+++ _front_page/taiwan-reception.en.md
@@ -0,0 +1,18 @@
+---
+heading: Taiwan Reception
+background: img/j_stockton_20160510_1698-4-mini.jpg
+position: -1
+menu: 2019 Reception
+classes: center-text
+target: tw-reception
+lang: en
+draft: false
+---
+
+## Sunday, June 2<sup>nd</sup> 2019 at 12pm
+
+### [Howard Hotel](http://www.howard-hotels.com.tw/en/taipei/home/)
+
+160 Ren Ai Rd.  
+Sec.3  
+Taipei, Taiwan 10657
--- webpack/utils/MobileCheck.js
+++ webpack/utils/MobileCheck.js
@@ -1,21 +1,11 @@
 'use strict';
 
 export const isMobile = {
-  Android: function() {
-    return navigator.userAgent.match(/Android/i);
-  },
-  BlackBerry: function() {
-    return navigator.userAgent.match(/BlackBerry/i);
-  },
-  iOS: function() {
-    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-  },
-  Opera: function() {
-    return navigator.userAgent.match(/Opera Mini/i);
-  },
-  Windows: function() {
-    return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-  },
+  Android: () => navigator.userAgent.match(/Android/i),
+  BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
+  iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
+  Opera: () => navigator.userAgent.match(/Opera Mini/i),
+  Windows: () => navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i),
   any: function() {
     return (
       isMobile.Android() ||
@@ -24,5 +14,5 @@ export const isMobile = {
       isMobile.Opera() ||
       isMobile.Windows()
     );
-  }
+  },
 };
